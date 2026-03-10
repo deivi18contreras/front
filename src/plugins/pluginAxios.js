a830667f6dbe8fbdea66f71c3571a081e5b316d6
deivi18contreras <deivibogoya@gmail.com>
2026-03-09 21:29:42 -0500

    Subiendo link del backend

--- src/plugins/pluginAxios.js
+++ src/plugins/pluginAxios.js
@@ -3,7 +3,7 @@ import { useAuthStore } from '../store/Auth.js'; // Asegúrate de que este es el
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000/api',
-  //baseURL: 'https://onrender.numerologia.com/api',
+  //baseURL: 'https://backend-production-4304.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
