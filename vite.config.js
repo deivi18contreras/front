b3f3b5ee43f15003c767cfc779d25acc06c22597
deivi18contreras <deivibogoya@gmail.com>
2026-03-09 21:44:15 -0500

    solucion de problemas

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: { transformAssetUrls }
-  }),
-  
-quasar({
+
+  base: './',
+
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+
+    quasar({
       sassVariables: fileURLToPath(
         new URL('./src/quasar-variables.sass', import.meta.url)
       )
     })
-],
+  ],
+
+  build: {
+    outDir: '../docs/numerologia'
+  }
+
 })
